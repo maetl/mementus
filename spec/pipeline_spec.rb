6d9db5e7a999d817fff9a4f5a533cdc7d7890286
Mark Rickerby <me@maetl.net>
2016-08-12 13:28:50 +1000

    Refactor the pipeline spec to ensure full API coverage

--- spec/pipeline_spec.rb
+++ spec/pipeline_spec.rb
@@ -39,87 +39,109 @@ describe 'pipeline api' do
     end
   end
 
-  it 'traverses from the given node id' do
-    pipeline = graph.n(1)
-    expect(pipeline.id).to eq(1)
-    expect(pipeline.first.id).to eq(1)
+  describe '#n' do
+    it 'traverses from the given node id' do
+      pipeline = graph.n(1)
+      expect(pipeline.id).to eq(1)
+      expect(pipeline.first.id).to eq(1)
+    end
+
+    it 'traverses from the given label match' do
+      pipeline = graph.n(:passage)
+      expect(pipeline.id).to eq([1, 2, 4])
+      expect(pipeline.first.id).to eq(1)
+    end
+
+    it 'traverses from the given prop match' do
+      pipeline = graph.n(name: 'one')
+      expect(pipeline.id).to eq(1)
+      expect(pipeline.first.id).to eq(1)
+    end
   end
 
-  it 'traverses from the given node match' do
-    pipeline = graph.n(name: 'one')
-    expect(pipeline.id).to eq(1)
-    expect(pipeline.first.id).to eq(1)
+  describe '#out' do
+    it 'traverses to outgoing nodes' do
+      pipeline = graph.n(1).out
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(2)
+    end
+
+    it 'traverses to outgoing nodes matching label' do
+      pipeline = graph.n(1).out(:passage)
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(2)
+    end
+
+    it 'traverses to outgoing nodes matching filter' do
+      pipeline = graph.n(1).out(name: 'two')
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(2)
+    end
+
+    it 'traverses to second-degree outgoing nodes' do
+      pipeline = graph.n(1).out.out
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(4)
+    end
   end
 
-  it 'traverses from the given label match' do
-    pipeline = graph.n(:passage)
-    expect(pipeline.id).to eq([1, 2, 4])
-    expect(pipeline.first.id).to eq(1)
+  describe '#out_e' do
+    it 'traverses to outgoing edges' do
+      pipeline = graph.n(1).out_e
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.to.id).to eq(2)
+    end
+
+    it 'traverses to outgoing edges matching label' do
+      pipeline = graph.n(1).out_e(:choice)
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.to.id).to eq(2)
+    end
+
+    it 'traverses to outgoing edges matching filter' do
+      pipeline = graph.n(1).out_e(name: 'e3')
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.to.id).to eq(2)
+    end
   end
 
-  it 'traverses to outgoing nodes' do
-    pipeline = graph.n(1).out
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.id).to eq(2)
+  describe '#in' do
+    it 'traverses to incoming nodes' do
+      pipeline = graph.n(2).in
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(1)
+    end
+
+    it 'traverses to incoming nodes matching label' do
+      pipeline = graph.n(2).in(:passage)
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(1)
+    end
+
+    it 'traverses to incoming nodes matching filter' do
+      pipeline = graph.n(2).in(name: 'one')
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.id).to eq(1)
+    end
   end
 
-  it 'traverses to outgoing nodes matching filter' do
-    pipeline = graph.n(1).out(:passage)
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.id).to eq(2)
-  end
+  describe '#in_e' do
+    it 'traverses to incoming edges' do
+      pipeline = graph.n(2).in_e
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.from.id).to eq(1)
+    end
 
-  it 'traverses to second-degree outgoing nodes' do
-    pipeline = graph.n(1).out.out
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.id).to eq(4)
-  end
+    it 'traverses to incoming edges matching label' do
+      pipeline = graph.n(2).in_e(:choice)
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.from.id).to eq(1)
+    end
 
-  it 'traverses to outgoing edges' do
-    pipeline = graph.n(1).out_e
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.to.id).to eq(2)
-  end
-
-  it 'traverses to outgoing edges matching filter' do
-    pipeline = graph.n(1).out_e(:choice)
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.to.id).to eq(2)
-  end
-
-  it 'traverses to incoming nodes' do
-    pipeline = graph.n(2).in
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.id).to eq(1)
-  end
-
-  it 'traverses to incoming nodes matching label' do
-    pipeline = graph.n(2).in(:passage)
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.id).to eq(1)
-  end
-
-  it 'traverses to incoming nodes matching filter' do
-    pipeline = graph.n(2).in(name: 'one')
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.id).to eq(1)
-  end
-
-  it 'traverses to incoming edges' do
-    pipeline = graph.n(2).in_e
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.from.id).to eq(1)
-  end
-
-  it 'traverses to incoming edges matching label' do
-    pipeline = graph.n(2).in_e(:choice)
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.from.id).to eq(1)
-  end
-
-  it 'traverses to incoming edges matching filter' do
-    pipeline = graph.n(2).in_e(name: 'e3')
-    expect(pipeline.all.count).to eq(1)
-    expect(pipeline.all.first.from.id).to eq(1)
+    it 'traverses to incoming edges matching filter' do
+      pipeline = graph.n(2).in_e(name: 'e3')
+      expect(pipeline.all.count).to eq(1)
+      expect(pipeline.all.first.from.id).to eq(1)
+    end
   end
 end
